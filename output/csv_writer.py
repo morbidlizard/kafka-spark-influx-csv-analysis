00e7ebfce71554996a96ff1598226c2e260d110d
Denis V. Ryabokon <ryabokon_dv@bw-sw.com>
2017-04-24 12:26:15 +0700

    IBDP-37: support writing object to csv

--- output/csv_writer.py
+++ output/csv_writer.py
@@ -1,3 +1,8 @@
+import pyspark
+import pandas
+
+from collections import Iterable
+from pyspark.sql import SparkSession
 from .output_writer import OutputWriter
 
 
@@ -6,7 +11,13 @@ class CSVWriter(OutputWriter):
         OutputWriter.__init__(self, path)
         self.sep = sep
         self.encoding = encoding
+        self.spark = SparkSession.builder.getOrCreate()
 
-    def write(self, rdd):
-        data = rdd.toPandas()
+    def write(self, rdd_or_object):
+        if isinstance(rdd_or_object, pyspark.rdd.RDD):
+            data = self.spark.createDataFrame(rdd_or_object).toPandas()
+        else:
+            data = pandas.DataFrame.from_records(
+                [rdd_or_object] if isinstance(rdd_or_object, Iterable) else [(rdd_or_object,)])
+
         data.to_csv(self.path, self.sep, self.encoding)
